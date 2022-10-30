bcda554e039ae1daf46b401d106e8d04d3074c93
gil <longlongroad@naver.com>
2022-10-30 11:53:45 +0900

    [fix] eslint

--- gil-react/src/App.js
+++ gil-react/src/App.js
@@ -4,16 +4,28 @@ import styles from "./App.module.css";
 
 function App() {
     const [counter, setValue] = useState(0);
+    const [keyword, setKeyword] = useState("");
     const onClick = () => setValue((prev) => prev + 1);
+    const onChangeValue = (e) => {
+        setKeyword(e.target.value);
+    }
 
     console.log("init");
     useEffect(() => {
         console.log('init once!');
     },[]);
+    useEffect(()=>{
+
+    });
 
     return (
       <div>
         <h1 className={styles.title}>{counter}</h1>
+        <input
+            type="text"
+            value={keyword}
+            onChange={onChangeValue}
+        />
         {/*<Btn text={"CONTINUE"} onClick={onClick}/>*/}
         <button onClick={onClick}>click</button>
       </div>
